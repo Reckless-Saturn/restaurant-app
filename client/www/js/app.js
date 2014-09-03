5dd3f64b2b84645efc545de298943b3f935e9f0c
David Nguyen <davidnguyen84@gmail.com>
2014-09-03 14:24:07 +0100

    (feat) Finish log-in page for customer - issue #17

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -135,6 +135,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
     ;
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/splash');
 });
 
