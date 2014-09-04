e32fc9b4203db1800767f75c8f5ba1509befed90
Ly Mai Le <lymaile@gmail.com>
2014-09-03 20:54:43 -0700

    (cleanup) Remove irrelevant commented code

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -9,20 +9,6 @@ angular.module('starter.controllers', ['starter.services'])
   $scope.getSearchResults = Customer.getSearchResults;
   $scope.chooseRestaurant = Customer.chooseRestaurant;
 
-  // // begin watching
-  // var watch = $cordovaGeolocation.watchPosition({ frequency: 1000 });
-  // watch.promise.then(function() { /* Not  used */ }, 
-  //   function(err) {
-  //     // An error occurred.
-  //   }, 
-  //   function(position) {
-  //     // Active updates of the position here
-  //     // position.coords.[ latitude / longitude]
-  // });
-
-  // // clear watch
-  // $cordovaGeolocation.clearWatch(watch.watchID)
-
 })
 
 .controller('RestaurantCtrl', function($scope, Restaurant) {
