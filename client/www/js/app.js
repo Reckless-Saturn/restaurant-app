67517f38b30aad9e916856843ea7bb97fac2f97e
Armando Perez <armandopmj@gmail.com>
2014-09-05 10:48:36 -0700

    'rebase'

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -1,4 +1,17 @@
+<<<<<<< HEAD
 angular.module('starter', ['ionic', 'starter.controllers'])
+=======
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', 
+  ['ionic', 
+  'starter.controllers',
+  'ngCordova.plugins.push' ])
+>>>>>>> Start Push Notifications
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -39,8 +52,13 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       url: "/customer/signup",
       views: {
         'menuContent' :{
+<<<<<<< HEAD
           templateUrl: "templates/customer/signup.html",
           controller: 'CustomerCtrl'
+=======
+          templateUrl: "templates/search.html",
+          controller: 'PushCtrl'
+>>>>>>> Start Push Notifications
         }
       }
     })
--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -9,6 +9,42 @@ angular.module('starter.controllers', ['starter.services'])
   $scope.getSearchResults = Customer.getSearchResults;
   $scope.chooseRestaurant = Customer.chooseRestaurant;
 
+<<<<<<< HEAD
+=======
+  // // begin watching
+  // var watch = $cordovaGeolocation.watchPosition({ frequency: 1000 });
+  // watch.promise.then(function() { /* Not  used */ }, 
+  //   function(err) {
+  //     // An error occurred.
+  //   }, 
+  //   function(position) {
+  //     // Active updates of the position here
+  //     // position.coords.[ latitude / longitude]
+  // });
+
+  // // clear watch
+  // $cordovaGeolocation.clearWatch(watch.watchID)
+
+<<<<<<< HEAD
+=======
+.controller('PushCtrl', function($scope, $stateParams, $cordovaPush) {
+  $scope.sendPush = function() {
+    $cordovaPush.register("someData"); //D: this is for debugging
+    console.log("In Push Controller");
+  }
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+>>>>>>> Start Push Notifications
+>>>>>>> Start Push Notifications
 })
 
 .controller('RestaurantCtrl', function($scope, Restaurant) {
